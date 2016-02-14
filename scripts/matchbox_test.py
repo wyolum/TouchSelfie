48c850c1a25e405ca8ae262546c32016af35e057
Justin Shaw <wyojustin@gmail.com>
2016-02-14 16:58:16 -0500

    added Entry to matchbox_test.py

--- scripts/matchbox_test.py
+++ scripts/matchbox_test.py
@@ -7,18 +7,26 @@ def launch_matchbox():
     if matchbox is None:
         matchbox = subprocess.Popen(["matchbox-keyboard"])
         b.config(command=kill_matchbox, text="kill")
+        t.focus_set()
 def kill_matchbox():
     global matchbox
     if matchbox is not None:
         matchbox.terminate()
-        b.config(command=launch_matchbox, text="launch")
-        matchbox = None
+        try:
+            b.config(command=launch_matchbox, text="launch")
+            matchbox = None
+        except:
+            pass
 
 
 r = Tk()
-l = Label(r, text="HERE")
-b = Button(r, text="Matchbox!", command=launch_matchbox)
+l = Label(r, text="Matchbox-Keybaord!")
+t = Entry(r, width=40);
+b = Button(r, text="Launch", command=launch_matchbox)
 
 l.pack()
 b.pack()
+t.pack()
 r.mainloop()
+
+kill_matchbox()
