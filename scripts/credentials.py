45b3a8b4d6ac80bb3dafd0bc062d3d64b1e2fcd8
Gene Blanchard <gblanchard4@gmail.com>
2017-01-28 19:20:26 -0600

    updated credentials

--- scripts/credentials.py
+++ scripts/credentials.py
@@ -12,28 +12,8 @@ from oauth2client.file import Storage
 
 
 def OAuth2Login(client_secrets, credential_store, email):
-    scope='https://picasaweb.google.com/data/'
-    user_agent='picasawebuploader'
-
-    storage = Storage(credential_store)
-    credentials = storage.get()
-    if credentials is None or credentials.invalid:
-        flow = flow_from_clientsecrets(client_secrets, scope=scope, redirect_uri='urn:ietf:wg:oauth:2.0:oob')
-        uri = flow.step1_get_authorize_url()
-        webbrowser.open(uri)
-        code = raw_input('Enter the authentication code: ').strip()
-        credentials = flow.step2_exchange(code)
-
-    if (credentials.token_expiry - datetime.utcnow()) < timedelta(minutes=5):
-        http = httplib2.Http()
-        http = credentials.authorize(http)
-        credentials.refresh(http)
-
-    storage.put(credentials)
-
-    gd_client = gdata.photos.service.PhotosService(source=user_agent,
-                                                   email=email,
-
-                                                   additional_headers={'Authorization' : 'Bearer %s' % credentials.access_token})
+    gauth = GoogleAuth()
+    gauth.LocalWebserverAuth()
+    gd_client = GoogleDrive(gauth)
 
     return gd_client
