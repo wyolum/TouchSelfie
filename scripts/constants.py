48363b4342e131a38d500d1d994d9a24e2db2326
laurentalacoque <laurent.alacoque@gmail.com>
2018-05-30 15:01:18 +0200

    Added some constants to adapt to different hardware

--- scripts/constants.py
+++ scripts/constants.py
@@ -2,6 +2,21 @@ EFFECTS = ["None", "Warhol", "Four"]
 
 SCREEN_W = 800 ## raspi touch
 SCREEN_H = 480 ## raspi touch
+
+#Raspicam v2 stills dimensions
+CAM_W = 3280
+CAM_H = 2464
+
+#Desired snapshot size
+SNAP_W = 1640
+SNAP_H = 1232
+#Desired size of the embedded logo
+LOGO_MAX_SIZE = 256
+
+# Snapshot to screen scale
+# Based on width
+SNAP_TO_SCREEN_SCALE = int(SNAP_W / SCREEN_W)
+
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
