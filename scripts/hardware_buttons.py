575ded01b143c36f4cdc800dad2ba406929845ab
laurentalacoque <laurent.alacoque@gmail.com>
2018-06-04 21:31:36 +0200

    First version of hardware buttons

---
+++ scripts/hardware_buttons.py
@@ -0,0 +1,33 @@
+'''
+    Basic interface for hardware buttons
+    Laurent Alacoque 2o18
+'''
+
+import RPi.GPIO as GPIO
+
+## GPIO pin definition for hardware buttons
+BUTTONs_PINS = [10,8,12]
+BUTTONS_MODE = GPIO.PUD_DOWN # switch to VDD, configure io in pulldown
+BUTTON_IS_ACTIVE = 1         # active 1 GPIO (pull_down with switch to VDD)
+
+
+class Buttons():
+    def __init__(self, buttons_pins=BUTTONS_PINS, mode=BUTTONS_MODE, active_state=BUTTON_IS_ACTIVE):
+        self.buttons_pins = buttons_pins
+        self.mode = mode
+        self.active_state = active_state
+        GPIO.setmode(GPIO.BOARD)
+        for pin in self.buttons:
+            GPIO.setup(pin, GPIO.IN, pull_up_down = self.mode)
+    def __del__(self):
+        GPIO.cleanup()
+    def buttons_state(self):
+        ret = 0 # no button pressed
+        for i,pin in enumerate(self.buttons_pins,1):
+            if GPIO.input(pin) == self.active_state:
+                return i
+        else :
+            # reached the end of pins, none active
+            return 0
+        
+        
