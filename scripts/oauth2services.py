2d561ee141a0b96f3dca8ea6a83626a44373c989
Laurent Alacoque <laurent.alacoque@cea.fr>
2019-02-01 13:32:01 +0100

    moved to logging

--- scripts/oauth2services.py
+++ scripts/oauth2services.py
@@ -22,6 +22,13 @@ from httplib2 import Http
 from oauth2client import file, client, tools
 from googleapiclient.errors import HttpError
 
+import logging
+logging.getLogger("googleapiclient.discovery_cache").setLevel(logging.WARNING)
+logging.getLogger("googleapiclient.discovery").setLevel(logging.WARNING)
+
+log = logging.getLogger(__name__)
+
+
 class OAuthServices:
     """Unique entry point for Google Services authentication"""
     def __init__(self, client_secret, credentials_store, username, enable_upload = True, enable_email = True):
@@ -60,7 +67,9 @@ class OAuthServices:
         """Refresh authentication
         
         returns: True if the refesh was successfull, False otherwise"""
+        log.debug("refresh: Refreshing authentication token")
         if not (self.enable_email or self.enable_upload): # if we don't want features, just return
+            log.debug("refresh: canceled: no service needed")
             return False
         cred = self.__oauth_login()
         if cred is None:    
@@ -71,12 +80,15 @@ class OAuthServices:
     def __oauth_login(self):
         if not (self.enable_email or self.enable_upload): # if we don't want features, just return
             return None
+        log.debug("__oauth_login: getting cached authentication token")
         credentials = self.credential_store.get()
         if credentials is None or credentials.invalid:
+            log.warning("__oauth_login: No valid credentials found, starting authorization flow")
             flow = client.flow_from_clientsecrets(self.client_secret, self.scopes)
             credentials = tools.run_flow(flow, self.credential_store)
 
         if (credentials.token_expiry - datetime.utcnow()) < timedelta(minutes=5):
+            log.debug("__oauth_login: caching period reached, refreshing token online")
             credentials.refresh(Http())
 
         self.credential_store.put(credentials)
@@ -86,7 +98,7 @@ class OAuthServices:
         if not self.enable_upload: #we don't want it
             return None
         credentials = self.__oauth_login()
-        return build('photoslibrary', 'v1', http=credentials.authorize(Http()))
+        return build('photoslibrary', 'v1', http=credentials.authorize(Http()), cache_discovery=False)
         
     def create_album(self, album_name = "New Album", add_placeholder_picture = False):
         """ Create a new album in user's photo library
@@ -95,16 +107,19 @@ class OAuthServices:
                 album_name (str): the album name (deflt: "New Album")
                 add_placeholder_picture (bool): add a small random colored placeholder image to the album (dflt: False)
         """
+        log.debug("create_album: Creating album '%s' (placeholder image : %s)"%(album_name,str(True)))
         if not self.enable_upload:
+            log.warning("create_album: Canceled album creation (enable_upload was set to False in constructor)")
             return {}
         client = self.__get_photo_client()
         try:
             res = client.albums().create(body={"album":{"title":album_name}}).execute()
+            log.info("create_album: Album %s created with id: %s"%(album_name,res["id"]))
             if add_placeholder_picture:
                 self.upload_picture("placeholder.png", album_id = res["id"], generate_placeholder_picture=True)
             return res["id"]
         except Exception as e:
-            print("Error while creating album %s:"%str(e))
+            log.error("create_album: Error while creating album %s:"%str(e))
             return None
         
 
@@ -114,23 +129,29 @@ class OAuthServices:
             - a list({"title": "the title", "id":"jfqmfjqsjklfaz"}) if as_title_id argument is True (default)
             - the album list
         """
+        log.debug("get_user_albums: Getting user photos albums")
         if not self.enable_upload:
+            log.warning("get_user_albums: Canceled album fetching (enable_upload was set to False)")
             return {}
         client = self.__get_photo_client()
         albums = []
         try:
+            log.debug("get_user_albums: Fetching first page of results")
             request = client.albums().list(pageSize=50,excludeNonAppCreatedData=exclude_non_app_created_data).execute()
+            log.debug("get_user_albums: => %d albums found",len(request["albums"]))
             albums.extend(request["albums"])
             while (request.get("nextPageToken",None) is not None) and (len(request.get("albums",[])) == 50):
-                print("loading next album page...")
+                log.debug("get_user_albums: Fetching next page of results")
                 request = client.albums().list(pageSize=50, pageToken = request["nextPageToken"],excludeNonAppCreatedData=exclude_non_app_created_data).execute()
+                log.debug("get_user_albums: => %d albums found",len(request["albums"]))
                 albums.extend(request["albums"])
         except KeyError:
             #no albums
-            return list()
+            log.error("get_users_album: Error while processing request")
+            return albums
         except Exception as e:
             import json
-            print("Error while fetching albums %s request result:%s"%(str(e),json.dumps(request,indent=4)))
+            log.error("get_users_album: Error while processing request: %s"%str(e))
             raise(e)
             
         
@@ -163,12 +184,16 @@ class OAuthServices:
                 if set to True, <filename> picture won't be used and a 32x32 colored picture will be used instead
                 This is usefull to create an album and upload a random picture to it so that it shows up in google photos
         """
+        log.debug("upload_picture(%s, album_id = %s , title='%s', caption = %s, generate_placeholder_picture = %s)"%(filename,str(album_id),str(title), str(caption), str(generate_placeholder_picture)))
         if not self.enable_upload:
+            log.debug("upload_picture: Canceled (service not configured)")
             return False
+            
         client = self.__get_photo_client()
         creds = self.__oauth_login()
         
         # Step I: post file binary and get Token
+        log.debug("upload_picture: Step I: uploading picture %s"%filename)
         file = os.path.basename(filename)
         url = 'https://photoslibrary.googleapis.com/v1/uploads'
         authorization = 'Bearer ' + creds.access_token
@@ -183,6 +208,7 @@ class OAuthServices:
         
         try:
             if generate_placeholder_picture:
+                log.debug("upload_picture: generating placeholder picture")
                 from PIL import Image
                 from random import randint
                 # creating test image
@@ -195,9 +221,12 @@ class OAuthServices:
             else:
                 with open(filename, "rb") as image_file:
                     filecontent=image_file.read()
+            log.info("upload_picture: uploading picture %s (%d bytes)"%(filename,len(filecontent)))
             (response,token) = http.request(url,method="POST",body=filecontent,headers=headers)
             if response.status != 200:
+                log.warning("upload_picture: response code for upload %d != 200"%response.status)
                 raise IOError("Error connecting to %s"%url)
+            log.debug("upload_picture: Successfully uploaded image with id:[%s]"%token)
 
             # Step II: reference file Item
             if isinstance(caption,str):
@@ -208,25 +237,29 @@ class OAuthServices:
             media_reference = dict(newMediaItems = [photo_item])
             if album_id is not None:
                 media_reference["albumId"] = album_id
+            
+            import json;log.debug("upload_picture: referencing picture with id: [%s]\n %s"%(token, json.dumps(media_reference,indent=4)))
             try:
                 try:
                     res = client.mediaItems().batchCreate(body=media_reference).execute()
                 except HttpError as e:
                     if "Invalid album ID" in str(e):
-                        print("ERROR, bad album, uploading to user's stream")
+                        log.error("upload_picture: album_id (%s) is not a valid album"%album_id)
+                        log.warning("upload_picture: retrying to reference uploaded image without an album")
                         #Album is invalid, try to upload to user stream instead
                         res = client.mediaItems().batchCreate(body=dict(newMediaItems=[{"simpleMediaItem": {"uploadToken": token}}])).execute()
                 if res["newMediaItemResults"][0]["status"]["message"] == "OK":
+                    log.debug("upload_picture: successfully uploaded image")
                     return True
                 else:
+                    log.warning("upload_picture: Unrecognized response")
                     return False
 
-                    
             except Exception as e:
-                print("Error while referencing:",e)
+                log.error("upload_picture: Error while referencing picture with id: %s (%s)"%(token,str(e)))
                 return False
         except Exception as e:
-            print("Error while uploading:",e)
+            log.error("upload_picture: Error while uploading picture: (%s)"%str(e))
             return False
         return False
  
@@ -239,20 +272,24 @@ class OAuthServices:
             body    (str) : body of the message
             attachment_file (str) : path to the file to be attached (or None)
         """
-            
+        log.debug("send_message(%s, '%s', '...', attachment_file=%s)"%(to, subject,str(attachment_file)))
         if not self.enable_email:
+            log.debug("send_message: canceled (enable_email is False)")
             return False
         credentials = self.__oauth_login()
         http = credentials.authorize(Http())
-        service = discovery.build('gmail', 'v1', http=http)
+        service = discovery.build('gmail', 'v1', http=http, cache_discovery=False)
+        
+        log.debug("send_message: creating message")
         message = self.__createMessage(self.username, to, subject, body, body, attachment_file=attachment_file)
         
         try:
+            log.debug("sending message")
             sent_message = (service.users().messages().send(userId="me", body=message).execute())
-            print('Message Id: %s' % sent_message['id'])
+            log.info('send_message: successfully sent message with id: %s' % sent_message['id'])
             return True
         except errors.HttpError as error:
-            print('An error occurred during send mail: %s' % error)
+            log.error("send_message: An error occurred during send mail: %s" % error)
             return False
         return True
 
@@ -317,6 +354,8 @@ class OAuthServices:
 
 def test():
     """ test email and uploading """
+    logging.basicConfig(level=logging.ERROR)
+
     username = raw_input("Please enter your email address: ")
     
     # creating test image
