a5b6ad6582fd4313beefe143ae13e33c8cb7b85a
Laurent Alacoque <laurent.alacoque@cea.fr>
2019-02-01 16:19:44 +0100

    Improved logging

--- scripts/fakehardware.py
+++ scripts/fakehardware.py
@@ -2,6 +2,8 @@
     dummy classes to fake hardware (for test only)
     Laurent Alacoque 2o18
 '''
+import logging
+log = logging.getLogger(__name__)
 
 from PIL import ImageDraw,ImageFont,Image
 
@@ -24,9 +26,10 @@ class PiCamera:
     '''
     def __init__(self):
         """Constructor of a fake PiCamera
-        
+
         Sets default values to populate the class properties
         """
+        log.warning("Creating a dummy PiCamera: no real picture will be taken")
         self.hw_state = 0
         self.resolution = (800, 600)
         self.frame_counter = 0
@@ -35,13 +38,16 @@ class PiCamera:
     def start_preview(self):
         """does nothing"""
         self.previewing = True
+        log.info("starting (fake) preview")
             
     def stop_preview(self):
         """does nothing"""
         self.previewing = False
+        log.info("stoping (fake) preview")
         
     def capture(self, filename, resize=None):
         """Generate a new test image"""
+        log.info("Generating dummy picture #%d"%self.frame_counter)
         im = Image.new('RGBA', self.resolution, (40,40,40,0))
         self.frame_counter = self.frame_counter + 1
         draw = ImageDraw.Draw(im)
@@ -50,12 +56,14 @@ class PiCamera:
             fnt = ImageFont.truetype('arial.ttf', 80)
             draw.text((10,10), "[%d]"%self.frame_counter, font = fnt, fill=(255,255,255,128))
         except IOError:
+            log.warning("font arial.ttf not found, using default")
             draw.text((10,10), "[%d]"%self.frame_counter, fill=(255,255,255,128))
         im = im.convert('RGB')
         im.save(filename)
         return True
         
     def add_overlay(self,overlay_image, size=(640,480)):
+        log.info("faking overlay")
         return FakeOverlay()
         pass
         
